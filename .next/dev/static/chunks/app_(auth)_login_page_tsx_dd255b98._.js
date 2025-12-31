2bbe03324e1042d03922e339088693ef240a1997
Jesus Ugarte <Jugarteosta@knights.ucf.edu>
2025-12-30 22:30:41 -0500

    Remove .next directory from git tracking

--- .next/dev/static/chunks/app_(auth)_login_page_tsx_dd255b98._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_21b0ffcd._.js",
-  "static/chunks/_78072314._.js"
-],
-    source: "dynamic"
-});
